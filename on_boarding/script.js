43a3491f8b0f348552a15c6452b4e541a19e7f74
ritik-xforia <ritik.t@xforia.com>
2025-02-03 21:23:03 +0530

    chanegs

--- on_boarding/script.js
+++ on_boarding/script.js
@@ -15,12 +15,16 @@ let childDeviceDetails = false
 
 export const options = {
   scenarios: {
-    rampUpTo100k: {
+    rampUpTo60k: {
       executor: 'ramping-vus',
       startVUs: 0, // Start with 0 virtual users
       stages: [
-        { duration: '10m', target: 80000 },  // Gradually ramp up to 50k VUs in 5 minutes
-        { duration: '10m', target: 80000 }, // Continue ramping up to 100k VUs over 10 minutes
+        { duration: '2m', target: 10000 }, // Ramp up to 10k VUs in 2 minutes
+        { duration: '2m', target: 20000 }, // Ramp up to 20k VUs in 2 minutes
+        { duration: '2m', target: 30000 }, // Ramp up to 30k VUs in 2 minutes
+        { duration: '2m', target: 40000 }, // Ramp up to 40k VUs in 2 minutes
+        { duration: '2m', target: 50000 }, // Ramp up to 50k VUs in 2 minutes
+        { duration: '2m', target: 60000 }, // Ramp up to 60k VUs in 2 minutes
       ],
     },
   },
@@ -28,7 +32,6 @@ export const options = {
     name: "load_test",  // Use a static name instead of dynamic high-cardinality values
   },
 };
-
 
 
 export default function() {
