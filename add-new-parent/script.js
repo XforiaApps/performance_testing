0e25f8fe139e9da3df54fd131b4d2a3f68564a43
ritik-xforia <ritik.t@xforia.com>
2025-02-09 14:12:56 +0530

    changes

--- add-new-parent/script.js
+++ add-new-parent/script.js
@@ -21,7 +21,7 @@ export const options = {
             executor: "constant-arrival-rate",
             rate: 7000, // ~417 users per second to reach 3,000,000 users in 2 hours
             timeUnit: "1s", // New users arrive every second
-            duration: "1m", // Test duration of 2 hours
+            duration: "5m", // Test duration of 2 hours
             preAllocatedVUs: 1000, // Pre-allocate enough VUs to handle the load
             maxVUs: 2000, // Allow up to 5000 VUs for peak concurrency
         },
@@ -102,12 +102,10 @@ export default function (userDetails) {
 
     const supervisorData = supervisorRes.json();
     const deepLinkSupervisor = supervisorData.deepLink;
-
     if (!deepLinkSupervisor) {
         console.error("Error: deepLinkSupervisor is missing in response");
         return;
     }
-    
     const tokenSupervisor = deepLinkSupervisor.match(/token=([^&]+)/)?.[1];
     const hashedName = deepLinkSupervisor.match(/nameHash=([^&]+)/)?.[1];
 
