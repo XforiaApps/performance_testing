fa26f3ab2fe4f398d287d8013e9c9c91a0850799
ritik-xforia <ritik.t@xforia.com>
2025-02-18 17:27:45 +0530

    changes

--- on_boarding/script.js
+++ on_boarding/script.js
@@ -19,7 +19,7 @@ export const options = {
       executor: 'ramping-vus',
       startVUs: 0, // Start with 0 virtual users
       stages: [
-        { duration: '5m', target: 50000 }, // Ramp up to 10k VUs in 2 minutes
+        { duration: '5m', target: 30000 }, // Ramp up to 10k VUs in 2 minutes
         // { duration: '5m', target: 50000 }, // Ramp up to 20k VUs in 2 minutes
       ],
     },
