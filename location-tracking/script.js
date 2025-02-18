815f044df9a0b74ad4b0ce891753e97946b33203
ritik-xforia <ritik.t@xforia.com>
2025-02-18 23:09:03 +0530

    changes

--- location-tracking/script.js
+++ location-tracking/script.js
@@ -22,7 +22,7 @@ export const options = {
             executor: "constant-arrival-rate",
             rate: 3000, // ~417 users per second to reach 3,000,000 users in 2 hours
             timeUnit: "1s", // New users arrive every second
-            duration: "1m", // Test duration of 2 hours
+            duration: "5m", // Test duration of 2 hours
             preAllocatedVUs: 1000, // Pre-allocate enough VUs to handle the load
             maxVUs: 2000, // Allow up to 5000 VUs for peak concurrency
         },
